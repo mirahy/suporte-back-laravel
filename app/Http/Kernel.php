bf0391b9c7e3d4357df8b8267664cd312969a4db
Mirahy Ribeiro Branco Fonseca <mirahyfonseca@ufgd.edu.br>
2023-09-01 11:52:15 -0400

    middleware cors somente nas rotas API

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,8 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\TrustProxies::class,
-        \App\Http\Middleware\Cors::class,
+        \App\Http\Middleware\TrustProxies::class,    
     ];
 
     /**
@@ -42,6 +41,7 @@ class Kernel extends HttpKernel
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\Cors::class,
         ],
     ];
 
